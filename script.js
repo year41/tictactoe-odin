e9a9dbc9bf84340697c657398b4efcb10f46fce1
qwerty <162951673+year41@users.noreply.github.com>
2026-06-06 16:59:14 +1000

    Create player one and two objects.

--- script.js
+++ script.js
@@ -6,17 +6,26 @@ function GameBoard() {
     for (let i = 0; i < 3; i++) {
         board[i] = [];
         for (let j = 0; j < 3; j++) {
-            board[i].push(j);
+            board[i].push("");
         };
     };
     return board;
+};
+
+function GameController(playerOne = "Player 1", playerTwo = "Player 2") {
+    const board = GameBoard();
+
+    const players = [{ name: playerOne, mark: "X" }, { name: playerTwo, mark: "O" } ]
+
+    return { board, players };
 }
 
 console.log(GameBoard());
+console.log(GameController());
+
 
 
 /* 
--Create players objects/factory
 - object to control the flow
 - display controller
  */
